6368d492d594878558d6e1f15e90e15757cbcaa4
Phoenix1255657 <narendra.phoenix01@gmail.com>
2026-03-10 19:32:05 +0530

    Updated vite.preview.config.ts

--- vite.preview.config.ts
+++ vite.preview.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: 'frontend',
   base: '/',
+  envDir: '../',
+  build: {
+    outDir: '../frontend/dist',
+    emptyOutDir: true,
+  },
   preview: {
     host: true,
     port: 4173,
