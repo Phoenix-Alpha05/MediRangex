61321caa677e23ab12e6065c5a480c2bb0cc9f43
Phoenix1255657 <narendra.phoenix01@gmail.com>
2026-03-10 19:57:45 +0530

    Added vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'frontend',
+  base: '/',
+  envDir: '../',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    strictPort: true,
+  }
+})
